4fefe1081d2bbffc32a0b83798648b5209038919
Bhupender Singh <bhupendersingh92054@gmail.com>
2026-01-30 23:41:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+const users = [
+  {
+    id: 1,
+    username: "bhupender",
+    email: "bhupender@gmail.com",
+    status: "active"
+  },
+  {
+    id: 2,
+    username: "rahul",
+    email: "rahul123@gmail.com",
+    status: "inactive"
+  },
+  {
+    id: 3,
+    username: "priya",
+    email: "priya.dev@gmail.com",
+    status: "active"
+  },
+  {
+    id: 4,
+    username: "amit",
+    email: "amit.tech@gmail.com",
+    status: "pending"
+  },
+  {
+    id: 5,
+    username: "neha",
+    email: "neha@gmail.com",
+    status: "blocked"
+  }
+];
+
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Server is running successfully!");
+});
+
+app.get("/user/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const user = users.find(u => u.id === id);
+
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
+
+  res.json(user);
+});
+
+
+app.post("/user/:id", (req, res)=>{
+    const id =req.params.id;
+    const user = req.body;
+
+    console.log("added succesfully")
+    console.log("ID:", id);
+    console.log("Body:", user);
+    
+    
+    res.send("User added successfully");
+    
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running`);
+});
